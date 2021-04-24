25a13226b2270aa51e47a0de4f66afdff45308ec
artstar10 <arthurnevesx@hotmail.com>
2021-04-24 20:20:22 -0300

    criado tabela de mensagens

---
+++ projeto_nlw/src/database/migrations/1619306392788-createMessages.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class createMessages1619306392788 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
